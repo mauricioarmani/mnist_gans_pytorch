709ad567465d8d04c80fa3166c6c13ae618b990e
mauricioarmani <mauricioxv@hotmail.com>
2018-03-19 11:14:21 -0300

    Remove unecessary code

--- loader.py
+++ loader.py
@@ -4,22 +4,6 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import datasets, transforms
 
 
-class Data(Dataset):
-    def __init__(self, X, Y):
-       super(Data, self).__init__()
-       self.X = X
-       self.Y = Y
-
-    def __getitem__(self, index):
-        return self.X[index], self.Y[index]
-
-    def __len__(self):
-        return len(self.X)
-
-    def __add__(self, other):
-        return ConcatDataset([self, other])
-
-
 def load_data(batch_size):
     transform = transforms.Compose([
         transforms.ToTensor(),
